f475267f56c5ec9955f8f9218c04fbcee0b9196e
fernando.caitano <fernando.caitano@RM377.RMF.local>
2025-07-25 14:29:58 -0300

    Principal solo con menú sin funcionalidad

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,70 @@
+package aplicacion;
+
+import java.util.Scanner;
+
+
+public class Principal {
+
+	// Crea atributo de conexión con el modelo utilizando una fachada
+	// falta hacer...
+	static Scanner scan = new Scanner(System.in);
+
+
+	//-------------------------------------
+	// Texto menu
+	//-------------------------------------
+	public static void mostrarMenu() {
+		
+        System.out.println("Menú principal-----------------------\n");
+        System.out.println("1. Listar alumnos");
+        System.out.println("2. Listar administradores");
+        System.out.println("3. Consultar alumno");
+        System.out.println("4. Alta alumno externo");
+        
+        System.out.println("90. Fin\n");
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	//-----------------------------------------
+	// Menú ppal
+	//-----------------------------------------
+	public static void main(String[] args) {
+		// Cargo objetos desde la BD
+		//inicializar();
+
+		//Menú principal
+		int opcion = 0;
+		do {
+			mostrarMenu();
+			opcion = scan.nextInt();
+			switch (opcion) {
+			case 1: 
+				break;
+			case 2: 
+				break;
+			case 3: 
+				break;
+			case 4: 
+				break;
+			case 90:
+				System.out.println("Fin del programa");
+				break;
+			default:
+				System.out.println("Opción inválida");
+			}
+			System.out.println();
+			
+		} while (opcion != 90);
+
+		scan.close();
+
+    }	
+
+
+}
